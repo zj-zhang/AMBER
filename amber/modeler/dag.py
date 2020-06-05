e98f6a506da34abaf63b35863c8cabb0465c3234
Zijun Zhang <zzj.zju@gmail.com>
2020-06-05 13:31:04 -0400

    fix for flatten_op and reward dimension

--- amber/modeler/dag.py
+++ amber/modeler/dag.py
@@ -1423,12 +1423,15 @@ class EnasConv1dDAG:
                 x = tf.nn.dropout(x, rate=dropout_placeholders[-1])
             with tf.variable_scope("fc"):
                 fc_units = self.stem_config['fc_units'] if 'fc_units' in self.stem_config else 1000
-                if flatten_op == 'global_avg_pool':
+                if flatten_op == 'global_avg_pool' or flatten_op == 'gap':
                     inp_c = x.get_shape()[-1].value
                     w = create_weight("w_fc", [inp_c, fc_units])
+                    print("gap w: %s" % w)
                 elif flatten_op == 'flatten':
                     inp_c = np.prod(x.get_shape()[1:]).value
                     w = create_weight("w_fc", [inp_c, fc_units])
+                else:
+                    raise Exception("Unknown fc string: %s" % flatten_op)
                 b = create_bias("b_fc", shape=[fc_units])
                 x = tf.matmul(x, w) + b
                 x = tf.nn.relu(x)
