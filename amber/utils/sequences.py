1be0a02357ed1ead9a6d9e6f04cec1fad6da1df5
evancofer <evancofer@gmail.com>
2020-06-29 13:26:47 -0400

    Added HDF5-backed genomes and their tests
    Also added scripts to convert old genomes to HDF5-backed genomes.

--- amber/utils/sequences.py
+++ amber/utils/sequences.py
@@ -3,9 +3,15 @@
 """
 This module provides the `Sequence` class, which is an abstract class
 that defines the interface for loading biological sequence data.
+
+TODO
+----
+- Abstract away the PYFAIDX interface, so that inheritence and extension of the
+    genome class is more straightforward.
 """
 import abc
 
+import h5py
 import numpy
 import pyfaidx
 
@@ -335,4 +341,154 @@ class EncodedGenome(EncodedSequence, Genome):
                     chrom=chrom, start=start, end=end, strand=strand)).astype(numpy.float64)
 
 
+class HDF5Genome(Genome):
+    """This class allows the user to query a Genome stored in an HDF5 file.
 
+    Parameters
+    ----------
+    input_path : str
+        Path to an HDF5 file.
+    in_memory : bool
+        Specifies whether the genome should be loaded from
+        disk and stored in memory.
+
+
+    Attributes
+    ----------
+    data : h5py.File
+        The HDF5 file containing the genome sequence. Alternatively,
+        this can be a `dict` object mapping chromosomes to sequences that
+        stores the file in memory.
+    in_memory : bool
+        Specified whether the genomic data is being stored in memory.
+    chrom_len_dict : dict
+        A dictionary mapping the chromosome names to their lengths.
+    """
+    def __init__(self, input_path, in_memory=False):
+        """
+        Constructs a new `HDF5Genome` object.
+        """
+        self.in_memory = in_memory
+        if in_memory is True:
+            f = h5py.File(input_path, "r")
+            self.data = dict()
+            for k in f.keys():
+                self.data[k] = f[k][:].tostring().decode("UTF-8")
+            f.close()
+        else:
+            self.data = h5py.File(input_path, "r")
+        self.chrom_len_dict = {k: len(self.data[k]) for k in self.data.keys()}
+
+    def get_sequence_from_coords(self, chrom, start, end, strand="+"):
+        """Fetches a string representation of a sequence at
+        the specified coordinates.
+
+
+        Parameters
+        ----------
+        chrom : str
+            Chromosome to query from.
+        start : int
+            First position in queried sequence.
+        end : int
+            One past the last position in the queried sequence.
+        strand : str
+            The strand to sample from.
+
+        Returns
+        -------
+        str
+            The sequence of bases occuring at the queried
+            coordinates.
+
+        Raises
+        ------
+        IndexError
+            If the coordinates are not valid.
+        """
+        if self.coords_are_valid(chrom, start, end, strand):
+            x = self.data[chrom][start:end]
+            if not self.in_memory:
+                x = x.tostring().decode("UTF-8")
+            if strand == "-":
+                x = x.translate(_DNA_COMP_TABLE)[::-1]
+            return x
+        else:
+            s = "Specified coordinates ({} to {} on \"{}\", strand of \"{}\") are invalid!".format(
+                start, end, chrom, strand)
+            raise IndexError(s)
+
+
+class EncodedHDF5Genome(EncodedGenome):
+    """
+    This class allows the user to specify an HDF5-backed encoded genome.
+
+    Parameters
+    ----------
+    input_path : str
+        Path to the HDF5 file.
+    in_memory : bool
+        Specifies whether the genome should be loaded from
+        disk and stored in memory.
+
+
+    Attributes
+    ----------
+    data : h5py.File or dict
+        The HDF5 file pointer or a dict containing the sequences in memory.
+    in_memory : bool
+        Specified whether the genomic data is being stored in memory.
+    chrom_len_dict : dict
+        A dictionary mapping the chromosome names to their lengths.
+    """
+    def __init__(self, input_path, in_memory=False):
+        """
+        Constructs a new `EncodedHDF5Genome` object.
+        """
+        self.in_memory = in_memory
+        if in_memory is True:
+            f = h5py.File(input_path, "r")
+            self.data = dict()
+            for k in f.keys():
+                self.data[k] = f[k][()]
+            f.close()
+        else:
+            self.data = h5py.File(input_path, "r")
+        self.chrom_len_dict = {k: self.data[k].shape[0] for k
+                               in self.data.keys()}
+
+    def get_sequence_from_coords(self, chrom, start, end, strand="+"):
+        """Fetches an array representation of a sequence at
+        the specified coordinates.
+
+
+        Parameters
+        ----------
+        chrom : str
+            Chromosome to query from.
+        start : int
+            First position in queried sequence.
+        end : int
+            One past the last position in the queried sequence.
+        strand : str
+            The strand to sample from.
+
+        Returns
+        -------
+        str
+            The sequence of bases occuring at the queried
+            coordinates.
+
+        Raises
+        ------
+        IndexError
+            If the coordinates are not valid.
+        """
+        if self.coords_are_valid(chrom, start, end, strand):
+            x = self.data[chrom][start:end].copy()
+            if strand == "-":
+                x = numpy.flip(numpy.flip(x, 0), 1)
+            return x.astype(numpy.float64)
+        else:
+            s = "Specified coordinates ({} to {} on \"{}\", strand of \"{}\") are invalid!".format(start, end, chrom, strand)
+            raise IndexError(s)
---
+++ bin/convert_fasta_to_encoded_h5py.py
@@ -0,0 +1,46 @@
+"""
+Author: Evan M. Cofer
+Created on: June 29, 2020
+"""
+import argparse
+import os
+
+import h5py
+import numpy
+import pyfaidx
+
+from amber.utils.sequences import EncodedGenome
+
+
+def convert_file(input_file, output_file):
+    g = EncodedGenome(input_file, in_memory=False)
+    h5 = h5py.File(output_file, "w")
+
+    # Convert.
+    for chrom, chrom_len in g.chrom_len_dict.items():
+        ds = h5.create_dataset(chrom, (chrom_len, 4), dtype=g.ALPHABET_TO_ARRAY['N'].dtype)
+        s = str(g.data[chrom]).upper()
+        for i in range(chrom_len):
+            ds[i, :] = g.ALPHABET_TO_ARRAY[s[i]]
+
+    # Close.
+    h5.close()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Convert fasta file to H5PY file with array for genome.")
+    parser.add_argument("--input-file", type=str, required=True, help="Path to FASTA input file.")
+    parser.add_argument("--output-file", type=str, required=True, help="Path to store H5PY file at")
+    args = parser.parse_args()
+
+
+    # Validate arguments.
+    for x in [args.input_file]:
+        if not os.path.exists(x):
+            s = 'Found no input file at {}'.format(x)
+            raise ValueError(s)
+    for x in [args.output_file]:
+        if os.path.exists(x):
+            s = 'Found output file already exists at {}'.format(x)
+            raise ValueError(s)
+    # Run command.
+    convert_file(args.input_file, args.output_file)
